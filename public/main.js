4b307f8775103fe28a192185444213639023d2b5
Sean Clark Hess <seanhess@gmail.com>
2012-11-02 13:54:58 -0600

    working with service

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 
 
-angular.module('uarbg2', [], function ($routeProvider) {
+angular.module('uarbg2', ['services'], function ($routeProvider) {
     $routeProvider.when('/test', {templateUrl: 'partials/test.html'})
     $routeProvider.when('/game', {templateUrl: 'partials/game.html', controller: GameCtrl})
     $routeProvider.otherwise({redirectTo: '/test'})
---
+++ public/services/Players.js
@@ -0,0 +1,64 @@
+// This service keeps track of all the current players (in an array), and merges moves into your stuff
+// Also lets you join
+
+angular.module('services',[])
+.factory('Players', function($rootScope) {
+
+  var channel = "fake"
+  var all = []
+  var myname = null
+
+  function join(player) {
+    myname = player.name
+
+    PUBNUB.subscribe({
+      channel: channel,
+      restore: false, // stay connected even when browser is closed
+      callback: function(data) {
+        $rootScope.$apply(function() {
+          if (data.action == "join") onJoin(data)
+          else if (data.action == "move") onMove(data)
+        })
+      },
+      disconnect: function(data) {
+        console.log("DISCONNECTED!")
+      },
+      reconnect: function(data) {
+        console.log("RECONNECT")
+      },
+      connect: function(data) {
+        console.log("Connected")
+        PUBNUB.publish({
+          channel: channel,
+          message: {action:"join", player: player}
+        })
+      }
+    })
+  }
+
+  function onJoin(data) {
+    var player = data.player
+    if (player.name == myname) {
+      players.current = player
+      console.log("ME", players.current)
+    }
+    player.x = 4
+    player.y = 2
+    all.push(player)
+    console.log(all, players.all)
+  }
+
+  function onMove(data) {
+
+  }
+
+  var players = { 
+    current: null, 
+    all: all, 
+    join: join 
+  }
+
+  return players
+
+})
+
