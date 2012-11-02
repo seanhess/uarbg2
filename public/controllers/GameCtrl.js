6de7513ef45784a427a0c99b3677e334d3f317a5
Jeff Poole <jeff@jeffpoole.net>
2012-11-02 17:11:04 -0600

    Initial code for missiles using firebase

--- public/controllers/GameCtrl.js
+++ public/controllers/GameCtrl.js
@@ -6,29 +6,46 @@ function GameCtrl($scope, Players) {
   $scope.position = function (player) {
     return {left: player.x * 30 + "px", top: player.y * 30 + "px"}
   }
+  $scope.missilePosition = function (missile) {
+    return {left: missile.x * 30 + "px", top: missile.y * 30 + "px"}
+  }
+
 
   $scope.keypress = function (e) {
       var left = 37,
           up = 38,
           right = 39, 
-          down = 40;
+          down = 40,
+          space = 32;
+      
+      if(e.keyCode === space) {
+        players.fireMissile(players.current)
+        console.log("Space hit, firing missile!")
+        return // so we don't fire players.move()
+      }
 
       if(e.keyCode === up) {
         players.current.y -= 1  
+        players.current.facing = "up"
+
       }
 
       if(e.keyCode === right) {
         players.current.x += 1  
+        players.current.facing = "right"
       }
 
       if(e.keyCode === down) {
         players.current.y += 1  
+        players.current.facing = "down"
       }
 
       if(e.keyCode === left) {
         players.current.x -= 1  
+        players.current.facing = "left"
       }
 
+      
       players.move(players.current)
   }
 
--- public/services/Players.js
+++ public/services/Players.js
@@ -9,20 +9,29 @@ angular.module('services')
   return function(gameId) {
 
     var gameRef = new FirebaseChannel("fake")
-    var playersRef = gameRef.child('players')
+    var playersRef = gameRef.child('players')    
+    var missilesRef = gameRef.child('missiles')
+
     var all = []
+    var allMissiles = []
     var myname = null
+
+    var XMAX = 16
+    var YMAX = 16
 
     function join(player) {
       myname = player.name
       player.x = 0
       player.y = 0
+      player.facing = "down"
 
       // ugly! figure out how to do rootScope.apply better!
       playersRef.child(player.name).set(player)
       playersRef.on('child_added', apply(onJoin))
       playersRef.on('child_changed', apply(onMove))
       playersRef.on('child_removed', apply(onQuit))
+      missilesRef.on('child_added', apply(onNewMissile))
+      missilesRef.on('child_removed', apply(onRemovedMissile))
     }
 
     function apply(f) {
@@ -44,6 +53,9 @@ angular.module('services')
 
     function onMove(remotePlayer) {
       var player = playerByName(remotePlayer.name)
+      if (!player) {
+        return console.log("Error, player not found: "+remotePlayer.name)
+      }
       player.x = remotePlayer.x
       player.y = remotePlayer.y
     }
@@ -52,6 +64,49 @@ angular.module('services')
       all = all.filter(function(p) {
         return (p.name != player.name)
       })
+    }
+    function fireMissile(player) {
+      var missile = {
+        x: player.x,
+        y: player.y,
+        direction: player.facing,
+        sourcePlayer: player.name
+      }
+      console.log("fireMissile()")
+      missilesRef.child(player.name).set(missile)
+    }
+
+    function onNewMissile(missile) {
+      allMissiles.push(missile)
+      console.log("onNewMissile()")
+      var missTimer
+      missTimer = setInterval(function () {
+        $rootScope.$apply( function() {
+          console.log("Missile timer missile.x="+missile.x+", missile.y="+missile.y)
+          if (missile.direction === "right") {
+            missile.x += 1
+          } else if (missile.direction === "left") {
+            missile.x -= 1
+          } else if (missile.direction === "up") {
+            missile.y -= 1
+          } else if (missile.direction === "down") {
+            missile.y += 1
+          }
+          if (missile.y < 0 || missile.y > YMAX || missile.x < 0 || missile.x > XMAX) {
+              var idx = allMissiles.indexOf(missile)
+              if (idx != -1) allMissiles.splice(idx,1);
+              console.log("Ending missile timer")
+              clearInterval(missTimer);
+              missilesRef.child(missile.sourcePlayer).remove();
+              return false; // I don't think setInterval cares about these
+          }
+          return true;
+        })
+      }, 1000);
+    }
+
+    function onRemovedMissile(missile) {
+      console.log("onRemovedMissile()")
     }
 
     function move(player) {
@@ -64,11 +119,18 @@ angular.module('services')
       })[0]
     }
 
+    function missileByPlayerName(name) {
+      return allMissiles.filter(function(p) {
+        return (p.sourcePlayer == name)
+      })[0]
+    }
+
     var players = { 
       current: null, 
       all: all, 
       join: join,
-      move: move
+      move: move,
+      fireMissile: fireMissile
     }
 
     return players
