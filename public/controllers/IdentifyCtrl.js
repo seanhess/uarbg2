ecf77c706a682c36bf63aa4b3dfdbfaa9afb07c3
Sean Clark Hess <seanhess@gmail.com>
2012-11-02 17:44:36 -0600

    select a player avatar

--- public/controllers/IdentifyCtrl.js
+++ public/controllers/IdentifyCtrl.js
@@ -10,6 +10,9 @@ function IdentifyCtrl($scope, Players, CurrentPlayer, $location) {
   // [ ] provide current players with avatars for that game
   $scope.players = players
 
+  // available avatars
+  $scope.avatars = ['player1']
+
   // [ ] Pick a name and avatar
   // set a service with the currently selected player. the name and avatar, etc
   // must be set by the time you get to game
@@ -17,10 +20,20 @@ function IdentifyCtrl($scope, Players, CurrentPlayer, $location) {
   // If game doesn't have a current player, then go back to the identify/matchmaking screen!
 
   $scope.join = function() {
-    // HACK
-    $scope.player.avatar = "player1"
+    if (!$scope.player || !$scope.player.avatar || !$scope.player.name)
+      return alert("Please select a valid name and an avatar")
+
     CurrentPlayer.player = $scope.player
     $location.path("/game/" + $scope.gameId)
+  }
+
+  $scope.selectAvatar = function(name) {
+    $scope.player = $scope.player || {}
+    $scope.player.avatar = name
+  }
+
+  $scope.isPlayerAvatar = function(name) {
+    return ($scope.player && $scope.player.avatar == name)
   }
 
   players.listen()
