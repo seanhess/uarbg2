d41d51f2ebe268efb9ba05bf0f86d0ba9651f976
Sean Clark Hess <seanhess@gmail.com>
2012-11-02 20:00:37 -0600

    working directive

--- public/directives/sprite.js
+++ public/directives/sprite.js
@@ -3,8 +3,21 @@ angular.module('directives')
 			return function(scope, element, attrs) {
 				// element is the player div
 				// attrs.sprite is "player1"
-				attrs.$observe("sprite", function(val) {
-					console.log(scope.player.facing + val)
-				});
+
+        scope.$watch(attrs.sprite, function(val) {
+          console.log("SPRITE", val)
+        })
+
+        scope.$watch(attrs.spriteFacing, function(val) {
+          console.log("FACING", val)
+        })
+
+        //function render() {
+					//console.log(scope.player.facing + val)
+        //}
+
+
+				//attrs.$observe("sprite", function(val) {
+				//});
 			}
 	})
