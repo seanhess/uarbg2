40d396793a02b27c0d233eb7cc5f968ecb15c416
Sean Clark Hess <seanhess@gmail.com>
2012-11-02 18:16:49 -0600

    fixed crazy key bugs

--- public/directives/keys.js
+++ public/directives/keys.js
@@ -2,6 +2,10 @@ angular.module('directives', [])
 	.directive('keypress', function($parse) {
 			return function(scope, element, attrs) {
 				// element is a jquery element
+
+        // focus the div
+        element.attr('tabindex', 0)
+
 				var onPress = $parse(attrs.keypress)
 				element.bind('keydown', function(e) {
 				    scope.$apply(function() {
