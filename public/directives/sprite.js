f1a41cb6dae0df0f6ace433899e12581d6906225
Will Robertson <shoxty@gmail.com>
2012-11-02 20:31:24 -0600

    updated sprite

--- public/directives/sprite.js
+++ public/directives/sprite.js
@@ -4,20 +4,47 @@ angular.module('directives')
 				// element is the player div
 				// attrs.sprite is "player1"
 
-        scope.$watch(attrs.sprite, function(val) {
-          console.log("SPRITE", val)
-        })
+			function setFacing(facing) {
+				var facing = (typeof facing == 'undefined') ? "down" : facing;
 
-        scope.$watch(attrs.spriteFacing, function(val) {
-          console.log("FACING", val)
-        })
+				if(facing == "up") {
+					element.css('backgroundPositionY', '-100px');
+				}
 
-        //function render() {
-					//console.log(scope.player.facing + val)
-        //}
+				if(facing == "right") {
+					element.css('backgroundPositionY', '-50px');
+				}
 
+				if(facing == "down") {
+					element.css('backgroundPositionY', '-150px');
+				}
 
-				//attrs.$observe("sprite", function(val) {
-				//});
+				if(facing == "left") {
+					element.css('backgroundPositionY', '0px');
+				}
 			}
+
+			function setSprite(sprite) {
+				if(sprite == 0) {
+					element.css('backgroundPositionX', '0');
+				}
+
+				if(sprite == 1) {
+					element.css('backgroundPositionX', '-50px');
+				}
+
+				if(sprite == 2) {
+					element.css('backgroundPositionX', '-100px');
+				}
+			}
+
+	        scope.$watch(attrs.sprite, function(val) {
+	          setSprite(val, scope.player.facing);
+	        })
+
+	        scope.$watch(attrs.spriteFacing, function(val) {
+	          setFacing(val);
+	        })
+
+		}
 	})
