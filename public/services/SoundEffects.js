292c6e786ef66e9d8fb4a98c7e465adec3510bfb
Sean Clark Hess <seanhess@gmail.com>
2012-11-03 16:23:23 -0600

    fixed a bug with winniing

--- public/services/SoundEffects.js
+++ public/services/SoundEffects.js
@@ -13,7 +13,8 @@ angular.module('services').factory('SoundEffects', function() {
   }
 
   var bgMusic = new Audio("/audio/g-style.mp3")
-  var underwater = new Audio("/audio/Underwater.mp3")
+  //var underwater = new Audio("/audio/Underwater.mp3")
+  var epic = new Audio("/audio/UnderwaterEpicBattle.mp3")
 
   function makeMusic(audio, seconds) {
     seconds = seconds || 0
@@ -28,6 +29,7 @@ angular.module('services').factory('SoundEffects', function() {
   // all parameters required
   function makeSound(audio, seconds, duration) {
     return function() {
+      audio.pause()
       // we don't want the canplay thing if we're going to play it after it's been really loaded.
       //$(audio).bind("canplay", function() {
         audio.currentTime = seconds
@@ -41,7 +43,9 @@ angular.module('services').factory('SoundEffects', function() {
 
   return {
     music: makeMusic(bgMusic, 8), // start at 8 seconds
-    levelUp: makeSound(underwater, 76, 2500)
+    explosion: makeSound(epic, 0, 750), // 1 - 1.5
+    rocket: makeSound(epic, 1, 500)
+    //levelUp: makeSound(underwater, 76, 2500)
   }
 
 })
